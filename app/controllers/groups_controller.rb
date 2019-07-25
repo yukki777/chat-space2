c51fdc178d29dd883a6a6cc11f6fb2aead2ffd96
yukki <konan297goo@gmail.com>
2019-07-25 18:24:17 +0900

    initial commit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,19 +1,22 @@
 class GroupsController < ApplicationController
   def new
+    # group　新規作成画面
+    @group = Group.new
+    # Groupクラスのいんスタンス作成　アソシエーション先　user　従属　とuser_group　支配
+    @group.users << current_user
   end
 
   def create
-    
-  end
-
-  def edit
-
-  end
-
-  def update
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: '『ぶっ殺すぶっ殺すと言ってるだけの仲良しクラブを作成しました！』'
+    else
+      render :new
+    end
   end
 
   private
-
-
+  def group_params
+    params.require(:group).permit(:name, {:user_ids => []})
+  end
 end
