c95dd543df580e7da9ec4db90a946d8196b68129
yukki <konan297goo@gmail.com>
2019-08-18 12:28:15 +0900

    AWS微修正

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :message do
     content {Faker::Lorem.sentence}
-    image {File.open("#{Rails.root}/public/images/test_image.jpg")}
+    image {File.open("#{Rails.root}/public/images/vr_game_mother_boy.png")}
     user
     group
   end
