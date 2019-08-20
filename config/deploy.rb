6b9a4a96a200e3d3d78539800738d0c655995def
yukki <konan297goo@gmail.com>
2019-08-20 15:42:16 +0900

    set :ssh_optionsを変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/.ssh/tengamaster3.pem']
+                  keys: ['~/.ssh/tengamaster3.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
