13e8f340a544566fd722167a5fae4a57d9bd9dc9
yukki <konan297goo@gmail.com>
2019-08-20 15:50:10 +0900

    set :repo_url修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.11.0"
 
 set :application, "chat-space2"
-set :repo_url, "git@example.com:yukki777/chat-space2.git"
+set :repo_url, "git.@github.com:yukki777/chat-space2.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
